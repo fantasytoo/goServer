5d80c105a28af37d7c38f6b7b13d1ecf7dd5d696
liaotingxue <575852993@qq.com>
2022-11-17 14:21:01 +0800

    A星寻路

--- main.go
+++ main.go
@@ -5,8 +5,21 @@
 **/
 package main
 
-import "go_test/findPath"
+import (
+	"go_test/findPath"
+	"math/rand"
+)
 
 func main() {
-	findPath.QuadFindPath()
+	closeChan := make(chan bool)
+	for i := 0; i < 100; i++ {
+		go find()
+	}
+	<-closeChan
+}
+
+func find() {
+	start := &findPath.Cell{X: rand.Int63n(999) + 1, Y: rand.Int63n(999) + 1}
+	end := &findPath.Cell{X: rand.Int63n(999) + 1, Y: rand.Int63n(999) + 1}
+	findPath.QuadFindPath(*start, *end)
 }
